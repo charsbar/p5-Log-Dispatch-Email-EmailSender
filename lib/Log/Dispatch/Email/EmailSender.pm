2ee48d45db0249391993364f715f61a7ff494503
keroyonn <keroyonn@gmail.com>
2010-08-02 06:18:39 +0900

    initial commit

---
+++ lib/Log/Dispatch/Email/EmailSender.pm
@@ -0,0 +1,32 @@
+package Log::Dispatch::Email::EmailSender;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Log::Dispatch::Email::EmailSender -
+
+=head1 SYNOPSIS
+
+  use Log::Dispatch::Email::EmailSender;
+
+=head1 DESCRIPTION
+
+Log::Dispatch::Email::EmailSender is
+
+=head1 AUTHOR
+
+keroyonn E<lt>keroyonn@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
